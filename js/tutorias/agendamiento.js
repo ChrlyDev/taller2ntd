25ec6e6de872ea158b0c17123d799795d5ad4c12
felipemadeit <felipemadeit@gmail.com>
2025-03-14 00:37:00 -0500

    Datos agregados al diccionario de las tutorías

--- js/tutorias/agendamiento.js
+++ js/tutorias/agendamiento.js
@@ -139,6 +139,146 @@ const tutoriasDatos = {
             "tema": "Robótica",
             "fecha-hora": "2025-06-15 10:30 AM",
             "asignada": false
+        },
+        {
+            "id": 21,
+            "tutor": "María",
+            "tema": "Geometría",
+            "fecha-hora": "2025-06-20 09:00 AM",
+            "asignada": false
+        },
+        {
+            "id": 22,
+            "tutor": "Luis",
+            "tema": "Trigonometría",
+            "fecha-hora": "2025-06-25 03:00 PM",
+            "asignada": false
+        },
+        {
+            "id": 23,
+            "tutor": "Andrea",
+            "tema": "Álgebra",
+            "fecha-hora": "2025-07-01 10:00 AM",
+            "asignada": true
+        },
+        {
+            "id": 24,
+            "tutor": "Javier",
+            "tema": "Cálculo",
+            "fecha-hora": "2025-07-05 11:30 AM",
+            "asignada": false
+        },
+        {
+            "id": 25,
+            "tutor": "Mónica",
+            "tema": "Física cuántica",
+            "fecha-hora": "2025-07-10 02:00 PM",
+            "asignada": false
+        },
+        {
+            "id": 26,
+            "tutor": "Roberto",
+            "tema": "Química orgánica",
+            "fecha-hora": "2025-07-15 04:30 PM",
+            "asignada": false
+        },
+        {
+            "id": 27,
+            "tutor": "Sara",
+            "tema": "Literatura clásica",
+            "fecha-hora": "2025-07-20 09:00 AM",
+            "asignada": true
+        },
+        {
+            "id": 28,
+            "tutor": "Pablo",
+            "tema": "Historia del arte",
+            "fecha-hora": "2025-07-25 10:30 AM",
+            "asignada": false
+        },
+        {
+            "id": 29,
+            "tutor": "Claudia",
+            "tema": "Biología molecular",
+            "fecha-hora": "2025-08-01 01:00 PM",
+            "asignada": false
+        },
+        {
+            "id": 30,
+            "tutor": "Daniel",
+            "tema": "Geografía física",
+            "fecha-hora": "2025-08-05 03:00 PM",
+            "asignada": false
+        },
+        {
+            "id": 31,
+            "tutor": "Lucía",
+            "tema": "Inglés avanzado",
+            "fecha-hora": "2025-08-10 11:00 AM",
+            "asignada": true
+        },
+        {
+            "id": 32,
+            "tutor": "Fernando",
+            "tema": "Filosofía moderna",
+            "fecha-hora": "2025-08-15 02:30 PM",
+            "asignada": false
+        },
+        {
+            "id": 33,
+            "tutor": "Elena",
+            "tema": "Economía internacional",
+            "fecha-hora": "2025-08-20 09:30 AM",
+            "asignada": false
+        },
+        {
+            "id": 34,
+            "tutor": "Raúl",
+            "tema": "Derecho penal",
+            "fecha-hora": "2025-08-25 04:00 PM",
+            "asignada": false
+        },
+        {
+            "id": 35,
+            "tutor": "Carmen",
+            "tema": "Psicología clínica",
+            "fecha-hora": "2025-09-01 10:00 AM",
+            "asignada": true
+        },
+        {
+            "id": 36,
+            "tutor": "Héctor",
+            "tema": "Estadística avanzada",
+            "fecha-hora": "2025-09-05 11:30 AM",
+            "asignada": false
+        },
+        {
+            "id": 37,
+            "tutor": "Isabel",
+            "tema": "Música clásica",
+            "fecha-hora": "2025-09-10 03:00 PM",
+            "asignada": false
+        },
+        {
+            "id": 38,
+            "tutor": "Gabriel",
+            "tema": "Robótica avanzada",
+            "fecha-hora": "2025-09-15 09:00 AM",
+            "asignada": false
+        },
+        {
+            "id": 39,
+            "tutor": "María",
+            "tema": "Astronomía avanzada",
+            "fecha-hora": "2025-09-20 02:00 PM",
+            "asignada": false
+        },
+        {
+            "id": 40,
+            "tutor": "Luis",
+            "tema": "Dibujo artístico",
+            "fecha-hora": "2025-09-25 04:30 PM",
+            "asignada": true
         }
     ]
 };
